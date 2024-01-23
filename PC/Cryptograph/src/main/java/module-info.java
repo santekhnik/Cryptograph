c299e943645d3298a69f8e3f582c98fd4e2648d2
vot47 <dendet94@gmail.com>
2024-01-23 15:01:16 +0200

    Setup-application-environment

---
+++ PC/Cryptograph/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ua.com.globallogic.cryptograph {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ua.com.globallogic.cryptograph to javafx.fxml;
+    exports ua.com.globallogic.cryptograph;
+}
